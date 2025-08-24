61329252039688a71254447892469646bb479477
rashtrapal143 <rashtraapalw143@gmail.com>
2025-08-24 20:52:07 +0530

    Updated Home page with login/signup buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import BasketImg from "../assets/basket.jpg";
+// import BasketImg from "../assets/Basket.jpg"
 
 export default function Home() {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -20,12 +20,12 @@ export default function Home() {
         <h2 className="mb-4">Hello, {user.userName} 👋</h2>
       ) : (
         <>
-          <img
+          {/* <img
             style={{ maxWidth: "100%", height: "300px" }}
             src={BasketImg}
             alt="Basket"
             className="mb-4"
-          />
+          /> */}
             {/* Product button always visible */}
       <Link to="/products" className="btn btn-warning btn-lg mb-4">
         🛒 View Products
